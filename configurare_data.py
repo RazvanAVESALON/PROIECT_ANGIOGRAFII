9543894305a8b19c2e4d26ad0069c9484ed593ab
Razvan <razave81@gmail.com>
2022-10-25 15:46:03 +0300

    Update configurare_data.py

--- configurare_data.py
+++ configurare_data.py
@@ -12,46 +12,42 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as T
 import torchmetrics 
+import glob
+import os 
+import json
 
 from tqdm import tqdm
 from UNet import UNet
 
 
-def create_dataset_csv(images_dir, csv_path):
-    """Generare csv cu caile catre imaginile de input si mastile de segmentare
+def create_dataset_csv(path_construct):
+    path_list={"images_path":[],"annotations_path":[],"frames":[],"patient":[],"acquisition":[]}
+    #frame_list={"frames"}
 
-    Args:
-        images_dir (str or pathlib.Path): calea catre directorul cu imagini de input
-        right_masks_dir (str or pathlib.Path): calea catre directorul cu masti de segmentare pt plamanul drept
-        left_masks_dir (str or pathlib.Path): calea catre directorul cu masti de segmentare pt plamanul stang
-        csv_path (str or pathlib.Path): calea si numele fisierul csv care va fi salvat
+    for patient in path_construct:
+        #print (image)
+        #x=os.path.join(image,r"*")
+        
+        x=glob.glob(os.path.join(patient,r"*"))
+        # print (x)
+        for acquisiton in x:
+            img=os.path.join(acquisiton,"frame_extractor_frames.npz")
+            annotations=os.path.join(acquisiton,"clipping_points.json")
+            with open (annotations) as f :
+                clipping_points=json.load(f)
 
-    Returns:
-        pandas.DataFrame: contine caile catre fiecare imagine de input sau masca de segmentare
-    """
+            for frame in clipping_points:
+                frame_int= int(frame)
+                
+                path_list['images_path'].append(img)
+                path_list['annotations_path'].append(annotations)
+                path_list['frames'].append(frame_int)
+                path_list['patient'].append(patient)
+                path_list['acquisition'].append(acquisiton)
+        
+            
 
-    # se citesc toate caile catre imagini si masti de segmentare
-    # este important sa fie in aceeasi ordine
-    images = sorted(list(pt.Path(images_dir).rglob("*.png")))
-    right_masks = sorted(list(pt.Path(right_masks_dir).rglob("*.png")))
-    left_masks = sorted(list(pt.Path(left_masks_dir).rglob("*.png")))
-
-    # se verifica daca nu exista masti lipsa pentru unul dintre plamani
-    assert len(right_masks) == len(left_masks), \
-         f"nr. of right lung masks {len(right_masks)} != {len(left_masks)} nr. of left lung masks"
-    
-    # se verifica daca nu exista imagini sau masti lipsa
-    assert len(images) == len(left_masks), \
-         f"nr. of image{len(images)} != {len(left_masks)} nr. of masks"
-
-    # se creaza un dictionar de liste, pe baza caruia se creaza obiectul de tip pandas.DataFrame
-    dataset_data = {"image_path": images, "right_lung_mask_path": right_masks, "left_lung_mask_path": left_masks}
-
-    dataset_df = pd.DataFrame(data=dataset_data)
-    dataset_df.to_csv(csv_path, index=False)
-    print(f"Saved dataset csv {csv_path}")
-
-    return dataset_df
+    return path_list
 
 def split_dataset(dataset_df, split_per, seed=1):
     """Impartirea setului de date in antrenare, validare si testare in mod aleatoriu
@@ -64,26 +60,28 @@ def split_dataset(dataset_df, split_per, seed=1):
     """
     # se amesteca aleatoriu indecsii DataFrame-ului
     # indexul este un numar (de cele mai multe ori) asociat fiecarui rand
-    indices = dataset_df.index.to_numpy() 
-    total = len(indices)
+    
+    
+    patients = dataset_df['patient'].unique()
+    
+    total = len(patients)
+    
     random.seed(seed)
-    random.shuffle(indices)
+    random.shuffle(patients)
 
     # se impart indecsii in functie de procentele primite ca input
     train_idx = int(total * split_per["train"])
     valid_idx = train_idx + int(total * split_per["valid"])
     test_idx = train_idx + valid_idx + int(total * split_per["test"])
 
-    train_indices = indices[:train_idx]
-    valid_indices = indices[train_idx:valid_idx]
-    test_indices = indices[valid_idx:test_idx]
-
-#     print(len(train_indices), len(valid_indices), len(test_indices))
-
-    # se adauga o noua coloana la DataFrame care specifica in ce subset face parte o imagine si mastile de segmentare asociate
+    train_patients = patients[:train_idx]
+    valid_patients = patients[train_idx:valid_idx]
+    test_patients = patients[valid_idx:test_idx]
+ 
     dataset_df['subset'] = ""
-    dataset_df.loc[train_indices, "subset"] = "train"
-    dataset_df.loc[valid_indices, "subset"] = "valid"
-    dataset_df.loc[test_indices, "subset"] = "test"
-
+    
+    dataset_df.loc[dataset_df['patient'].isin(train_patients), 'subset'] = 'train'
+    dataset_df.loc[dataset_df['patient'].isin(valid_patients), 'subset'] = 'valid'
+    dataset_df.loc[dataset_df['patient'].isin(test_patients), 'subset'] = 'test'
+    
     return dataset_df
