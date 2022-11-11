5a1b7b825f83ed2aeae689d7db6255fc987ba70a
Razvan <razave81@gmail.com>
2022-11-11 11:45:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -26,33 +26,54 @@ from angio_class import AngioClass
 
 from torchmetrics import Dice
 
+# def (dataset_df):
+    
+#     for i in range()
+
 
 def test(network, test_loader, thresh=0.5):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"[INFO] Starting testing on device {device} ...")
 
     metric =  Dice(average='micro')
-
+    dice_CSV={'Dice':[]}
     network.eval()
     with tqdm(desc='test', unit=' batch', total=len(test_loader.dataset)) as pbar:
+        
         for data in test_loader:
-            ins, tgs = data
+            ins, tgs= data
+            network.to(device)
             ins = ins.to(device)
             tgs = tgs.to('cpu')
-
             output = network(ins)
+         
             current_predict = (F.softmax(output, dim=1)[:, 1] > thresh)
 
+           
             if 'cuda' in device.type:
                 current_predict = current_predict.cpu()
-                
-            dice = metric(current_predict, tgs.squeeze())
+            
+            print (current_predict.shape, tgs.shape)
+            for (frame_pred ,target_pred) in zip(current_predict , tgs):  
+                dice = metric(frame_pred, target_pred.squeeze())
+                dice_CSV['Dice'].append(dice)
             pbar.update(ins.shape[0])
         
         dice = metric.compute()
+        
         print(f'[INFO] Test accuracy is {dice*100:.2f} %')
+        return dice_CSV
 
 
+# def csvdata (dataset_df):
+#     data={'patients':[],'acquisition':[],'frames':[]}
+#     for id in range(len(dataset_df['patient'])):
+#         data['patients'].append(dataset_df['patient'][id])
+#         data['acquisition'].append(dataset_df['acquisition'][id])
+#         data['frame'].append(dataset_df['frames'][id])
+
+    
+#     return data
 
    
 def main():
@@ -63,7 +84,7 @@ def main():
         
     yml_data=yaml.dump(config)
     directory =f"Test{datetime.now().strftime('%m%d%Y_%H%M')}"
-    parent_dir =r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\Experiment_Dice_index11012022_1644"
+    parent_dir =r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\Experiment_Dice_index11072022_2316"
     path=pt.Path(parent_dir)/directory
     path.mkdir(exist_ok=True)
 
@@ -71,69 +92,79 @@ def main():
     f.write(yml_data)    
 
     path_construct=glob.glob(r"E:\__RCA_bif_detection\data\*")
-    path_list=create_dataset_csv(path_construct)
-    dataset_df = pd.DataFrame(path_list)  
-    dataset_df.to_csv(r'D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\CSV_angiografii.csv')  
+    
+    #path_list=create_dataset_csv(path_construct)
+    #dataset_df = pd.DataFrame(path_list)  
+   
         
-    dataset_df = split_dataset(dataset_df, split_per=config['data']['split_per'], seed=1)
-    print (dataset_df.head(3))
-    dataset_df.to_csv(r'D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\CSV_angiografii.csv')  
-
-    geometrics = T.Compose([
-            
-            T.ToPILImage(),
-            T.ToTensor(),
-    ])
+    #dataset_df = split_dataset(dataset_df, split_per=config['data']['split_per'], seed=1)
+    #print (dataset_df.head(3))
     
-    pixels=T.Compose([
-            
-            T.ToPILImage(),
-            T.ToTensor(),
-    ])
     
+    dataset_df=pd.read_csv(config['data']['dataset_csv'])  
     test_df = dataset_df.loc[dataset_df["subset"] == "test", :]
-    test_ds = AngioClass(test_df, img_size=config["data"]["img_size"], geometric_transforms=geometrics,pixel_transforms=pixels)
+    test_ds = AngioClass(test_df, img_size=config["data"]["img_size"])
+    print(test_ds[0])
+    
     test_loader = torch.utils.data.DataLoader(test_ds, batch_size=config["train"]["bs"], shuffle=False)
     
-    network = torch.load(r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\Experiment_Dice_index11012022_1644\Weights\my_model11012022_1932_e95.pt")
+    print (test_loader.dataset[0])
+    #print (AngioClass.__csvdata__())
+    
+    network = torch.load(r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\Experiment_Dice_index11072022_2316\Weights\my_model11082022_0223_e90.pt")
 
     print(f"# Test: {len(test_ds)}")
 
 
-    test(network, test_loader, thresh=config['test']['threshold'])
-
-    x, y = next(iter(test_loader))
-    network.eval()
-    y_pred = network(x.to(device='cuda'))
-    y_pred.shape
-
-    nr_exs = 4 # nr de exemple de afisat
-    fig, axs = plt.subplots(nr_exs, 3, figsize=(10, 10))
-    for i, (img, gt, pred) in enumerate(zip(x[:nr_exs], y[:nr_exs], y_pred[:nr_exs])):
-        axs[i][0].axis('off')
-        axs[i][0].set_title('Input')
-        axs[i][0].imshow(img[0], cmap='gray')
-
-        axs[i][1].axis('off')
-        axs[i][1].set_title('Ground truth')
-        axs[i][1].imshow(gt[0], cmap='gray')
-
-        # print(pred.shape)
-        pred = F.softmax(pred, dim=0)[1].detach().cpu().numpy()
-        # print(pred.shape, pred.min(), pred.max())
-        pred[pred > config['test']['threshold']] = 1
-        pred[pred <= config['test']['threshold']] = 0
-        pred = pred.astype(np.uint8)
-        
+    test_set_CSV=test(network, test_loader, thresh=config['test']['threshold'])
     
-        axs[i][2].axis('off')
-        axs[i][2].set_title('Prediction ')
-        axs[i][2].imshow(pred, cmap='gray')
-        
-        plt.savefig(f"{path}\\Măști") 
+    test_df['Dice']=test_set_CSV['Dice']
     
-    
+    test_CSV=pd.DataFrame(test_df)
+    test_CSV.to_csv(r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\Experiment_Dice_index11072022_2316\CSV_TEST.csv")
+
+   
+    for batch_index,batch in enumerate(test_loader):
+        x, y = iter(batch)
         
+        network.eval()
+        x=x.type(torch.cuda.FloatTensor)
+        y_pred = network(x.to(device='cuda:0'))
+        print( y_pred.shape)
+
+        
+        fig, axs = plt.subplots(3, figsize=(50, 50))
+       
+        print (len(x),x.shape)
+       
+        for  step , (img, gt, pred) in enumerate(zip(x, y, y_pred)):
+            print (pred.shape, img.shape,gt.shape)
+            axs[0].axis('off')
+            axs[0].set_title('Input')
+            axs[0].imshow(img[0].cpu(), cmap='gray')
+    
+            axs[1].axis('off')
+            axs[1].set_title('Ground truth')
+            axs[1].imshow(gt[0], cmap='gray')
+
+            # print(pred.shape)
+            pred = F.softmax(pred,dim=0)[1].detach().cpu().numpy()
+            # print(pred.shape, pred.min(), pred.max())
+            pred[pred > config['test']['threshold']] = 1
+            pred[pred <= config['test']['threshold']] = 0    
+            pred = pred.astype(np.uint8)
+            
+            print (pred.shape)
+                    
+            axs[2].axis('off')
+            axs[2].set_title('Prediction ')
+            print (pred.shape)
+            axs[2].imshow(pred, cmap='gray')
+                    
+            plt.savefig(f"{path}\\Măști_frame{batch_index}_{step}.png") 
+                
+        
+            
         
 
 
