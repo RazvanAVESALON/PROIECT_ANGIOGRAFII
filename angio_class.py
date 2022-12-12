bb409b018500edcc6cdfa52b47c117e5fa3c9fa0
Razvan <razave81@gmail.com>
2022-12-12 12:13:39 +0200

    Update angio_class.py

--- angio_class.py
+++ angio_class.py
@@ -35,33 +35,57 @@ class AngioClass(torch.utils.data.Dataset):
 
         return len(self.dataset_df)
     
-    # def csvdata (self):
+    def csvdata (self,idx):
         
-    #     for index in range(len(self.dataset_df)):
-    #         patient=self.dataset_df['patient'][index]
-    #         acquisition=self.dataset_df['acquisition'][index]
-    #         frame=self.dataset_df['frames'][index]
         
-    #     print (frame)
-    #     return patient, acquisition, frame
+        patient=self.dataset_df['patient'][idx]
+        acquisition=self.dataset_df['acquisition'][idx]
+        frame=self.dataset_df['frames'][idx]
+        
+        print (frame)
+        return patient, acquisition, frame
+    
+    def crop_colimator(self, frame,gt, info):
+        img = frame.astype(np.float32)
+        in_min = 0
+        in_max = 2 ** info['BitsStored'] - 1
+        out_min = 0
+        out_max = 255
+        if in_max != out_max:
+            img = img.astype(np.float32)
+            img = (img - in_min) * ((out_max - out_min) / (in_max - in_min)) + out_min
+            img = np.rint(img)
+            img.astype(np.uint8)
+            
+        # crop collimator
+        img_edge = info['ImageEdges']
+        img_c = img[..., img_edge[2]:img_edge[3]+1, img_edge[0]:img_edge[1]+1]
+        new_gt=gt[..., img_edge[2]:img_edge[3]+1, img_edge[0]:img_edge[1]+1]
+        
+        return img_c,new_gt 
+        
+
     
     def __getitem__(self, idx):
         img = np.load(self.dataset_df['images_path'][idx])['arr_0']
-       
-
         frame_param = self.dataset_df['frames'][idx]
         new_img = img[frame_param]
-      
-        new_img = cv2.resize(img[frame_param], self.img_size, interpolation=cv2.INTER_AREA)
-        new_img = new_img*1/255
-
+        with open(self.dataset_df['angio_loader_header'][idx]) as f:
+            angio_loader = json.load(f)
+            
         with open(self.dataset_df['annotations_path'][idx]) as f:
             clipping_points = json.load(f)
 
         target = np.zeros(img.shape, dtype=np.uint8)
         target[frame_param] = cv2.circle(target[frame_param], [clipping_points[str(frame_param)][1], clipping_points[str(frame_param)][0]], 8, [255, 255, 255], -1)
-        new_target = cv2.resize( target[frame_param], self.img_size, interpolation=cv2.INTER_AREA)
+        
+        croped_colimator_img,croped_colimator_gt=self.crop_colimator(new_img,target[frame_param],angio_loader)
+        
+        new_img = cv2.resize(croped_colimator_img, self.img_size, interpolation=cv2.INTER_AREA)
+        new_target = cv2.resize(croped_colimator_gt, self.img_size, interpolation=cv2.INTER_AREA)
+        
         new_target = new_target/255
+        new_img = new_img*1/255
          
         x = np.expand_dims(new_img, axis=0)
         y = np.expand_dims(new_target, axis=0)
@@ -86,10 +110,10 @@ class AngioClass(torch.utils.data.Dataset):
  
         #print (tensor_x.min(),tensor_y.min(),tensor_x.max(),tensor_y.max())
        
-        # plt.imshow(tensor_x[0], cmap="gray")
-        # plt.show()
-        # plt.imshow(tensor_y[0] , cmap="gray")
-        # plt.show()
+        #plt.imshow(tensor_x[0], cmap="gray")
+        #plt.show()
+        #plt.imshow(tensor_y[0] , cmap="gray")
+        #plt.show()
 
         return tensor_x.float(), tensor_y.int()
     
