4e92d7f4a7407c7c5a5f72d54aea1cf7916b7095
Razvan <razave81@gmail.com>
2023-07-25 19:03:06 +0300

    Update blob_detector.py

--- utils/blob_detector.py
+++ utils/blob_detector.py
@@ -3,11 +3,11 @@ import cv2
 from skimage.feature import blob_doh
 import matplotlib.pyplot as plt
 import numpy as np
-
+from skimage.color import rgb2gray
 
 def blob_detector(img):
 
-    #image_gray = rgb2gray(img)
+    img = rgb2gray(img)
     img = img.astype(np.float64)
     # print(image_gray,image_gray.shape,image_gray.dtype,image_gray.max())
     blobs_doh = blob_doh(img, max_sigma=30, threshold=.01)
@@ -21,17 +21,17 @@ def blob_detector(img):
         y, x, r = blob
         cords_list["x"].append(x)
         cords_list["y"].append(y)
-        #c = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
-        # axes.add_patch(c)
+        c = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
+        axes.add_patch(c)
 
-    # plt.tight_layout()
-    # plt.show()
+    #plt.tight_layout()
+    plt.show()
     return cords_list
 
 
 def main():
     img = cv2.imread(
-        r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\PREDICTIE_14_3.png")
+        r"C:\Users\razav\OneDrive\Desktop\Poze documentatie licenta\PREDICTIE_00e91486f2fd40f198e7e09216937155_48809182-4.png")
     print(img, img.shape, img.dtype)
     list = blob_detector(img)
 
