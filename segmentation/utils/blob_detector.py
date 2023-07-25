4bfaaf283ae4045aaa8547a3bafdb7eba17fff5c
Razvan <razave81@gmail.com>
2023-07-25 19:02:46 +0300

    Update blob_detector.py

--- segmentation/utils/blob_detector.py
+++ segmentation/utils/blob_detector.py
@@ -7,25 +7,20 @@ import numpy as np
 
 def blob_detector(img):
 
-    #image_gray = rgb2gray(img)
+    #schimbarea tipului de date a imagini 
     img = img.astype(np.float64)
-    # print(image_gray,image_gray.shape,image_gray.dtype,image_gray.max())
+    #inițializarea funcției de detecție a puncte
     blobs_doh = blob_doh(img, max_sigma=30, threshold=.01)
     cords_list = {"x": [], "y": []}
-    #print (blobs_doh)
-    fig, axes = plt.subplots()
-    plt.title('Determinant of Hessian')
-    plt.imshow(img)
+ 
     for blob in blobs_doh:
-        #print (blob)
+      
         y, x, r = blob
+        # crearea listă cu punctele de bifucație
         cords_list["x"].append(x)
         cords_list["y"].append(y)
-        #c = plt.Circle((x, y), r, color='blue', linewidth=2, fill=False)
-        # axes.add_patch(c)
-
-    # plt.tight_layout()
-    # plt.show()
+       
+  
     return cords_list
 
 
@@ -34,6 +29,7 @@ def main():
         r"D:\ai intro\Angiografii\PROIECT_ANGIOGRAFII\PREDICTIE_14_3.png")
     print(img, img.shape, img.dtype)
     list = blob_detector(img)
+    
 
 
 if __name__ == "__main__":
